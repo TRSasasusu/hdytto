34035ad7af43a17ec079d6cf46ab96609ddac528
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2022-04-03 00:45:43 +0900

    dowhile test ok

--- hdytto/dowhile.py
+++ hdytto/dowhile.py
@@ -14,49 +14,20 @@ def dowhile(a):
     for type, name in a:
         #print(tok_name[type], name)
         l.append(Token(type, name))
-        print(l)
-        print(stack)
+        #print(l)
+        #print(stack)
         if checking_while_start_index is not None:
             if l[-1].name == ':':
                 checking_while_start_index = None
             elif l[-1].name == '\n':
-                while_condition_tokens = [Token(tokenize.NAME, 'if'), Token(tokenize.NAME, 'not'), Token(tokenize.OP, '(')] + l[checking_while_start_index + 1:-1] + [Token(tokenize.OP, ')'), Token(tokenize.OP, ':'), Token(tokenize.NEWLINE, '\n')]
-                i = 0
-                while True:
-                    if l[i].name == 'continue' or i == checking_while_start_index:
-                        for j, while_condition_token in enumerate(while_condition_tokens):
-                            l.insert(i + j, while_condition_token)
-                        """
-                        if i > 0 and l[i - 1].type == tokenize.INDENT:
-                            l.insert(i + j + 1, Token(tokenize.INDENT, l[i - 1] + '    '))
-                            l.insert(i + j + 2, Token(tokenize.NAME, 'break'))
-                            l.insert(i + j + 3, Token(tokenize.DEDENT, ''))
-                            if l[i + j + 4].name != 'continue':
-                                while len(l) > i + j + 4:
-                                    del l[i + j + 4]
-                                checking_while_start_index = None
-                                break
-                            else:
-                                l.insert(i + j + 4, Token(tokenize.INDENT, l[i - 1]))
-                                i += j + 5
-                                checking_while_start_index += j + 4
-                        else:
-                        """
-                        l.insert(i + j + 1, Token(tokenize.INDENT, '                '))
-                        l.insert(i + j + 2, Token(tokenize.NAME, 'break'))
-                        l.insert(i + j + 3, Token(tokenize.NEWLINE, '\n'))
-                        l.insert(i + j + 4, Token(tokenize.DEDENT, ''))
-                        if l[i + j + 5].name != 'continue':
-                            while len(l) > i + j + 5:
-                                del l[i + j + 5]
-                            checking_while_start_index = None
-                            break
-                        else:
-                            i += j + 5
-                            checking_while_start_index += j + 4
-                    i += 1
+                dowhile_condition_tokens = [Token(tokenize.NAME, 'if'), Token(tokenize.NAME, f'_HDYTTO_DOWHILE_COUNTER_{len(stack)}'), Token(tokenize.NAME, 'and'), Token(tokenize.NAME, 'not'), Token(tokenize.OP, '(')] + l[checking_while_start_index + 1:-1] + [Token(tokenize.OP, ')'), Token(tokenize.OP, ':'), Token(tokenize.NEWLINE, '\n'), Token(tokenize.INDENT, l[8].name + '    '), Token(tokenize.NAME, 'break'), Token(tokenize.NEWLINE, '\n'), Token(tokenize.DEDENT, ''), Token(tokenize.NAME, f'_HDYTTO_DOWHILE_COUNTER_{len(stack)}'), Token(tokenize.VBAREQUAL, '|='), Token(tokenize.NAME, 'True'), Token(tokenize.NEWLINE, '\n')]
+                for i, token in enumerate(dowhile_condition_tokens):
+                    l.insert(9 + i, token)
+                while len(l) > checking_while_start_index + len(dowhile_condition_tokens):
+                    del l[checking_while_start_index + len(dowhile_condition_tokens)]
+                checking_while_start_index = None
                 if len(stack) > 1:
-                    stack[-2].extend(l)
+                    stack[-2].extend(stack.pop(-1))
                     l = stack[-1]
                 else:
                     print('######')
@@ -70,8 +41,14 @@ def dowhile(a):
         if len(l) <= 1:
             continue
         if l[-2].name == 'do' and l[-1].name == ':':
+            l.pop(-1)
+            l.pop(-1)
             l = []
             stack.append(l)
+            l.append(Token(tokenize.NAME, f'_HDYTTO_DOWHILE_COUNTER_{len(stack)}'))
+            l.append(Token(tokenize.OP, '='))
+            l.append(Token(tokenize.NAME, 'False'))
+            l.append(Token(tokenize.NEWLINE, '\n'))
             l.append(Token(tokenize.NAME, 'while'))
             l.append(Token(tokenize.NAME, 'True'))
             l.append(Token(tokenize.OP, ':'))
--- tests/test_dowhile.py
+++ tests/test_dowhile.py
@@ -5,12 +5,33 @@ import unittest
 class TestDowhile(unittest.TestCase):
     def test_dowhile(self):
         a = 5
-        print(a)
         do:
             a += 5
             while a < 3
 
         self.assertEqual(10, a)
+
+        do:
+            b = -5
+            while a < 25:
+                a += 10
+            while b != -5
+
+        self.assertEqual(30, a)
+        self.assertEqual(-5, b)
+
+        do:
+            b *= 2
+            do:
+                a += b
+                c = a * 2
+                while c > 20
+            b += c
+            while b < 0
+
+        self.assertEqual(10, a)
+        self.assertEqual(10, b)
+        self.assertEqual(20, c)
 
 if __name__ == '__main__':
     unittest.main()
