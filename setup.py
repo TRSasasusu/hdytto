69d9c9e382d77a11a2f22bf8e03f53a17775859a
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2022-04-12 23:38:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = []
 
 setuptools.setup(
     name='hdytto',
-    version='0.1',
+    version='0.2',
     description='Add not pythonic syntax into your python.',
     long_description=long_description,
     long_description_content_type="text/markdown",
